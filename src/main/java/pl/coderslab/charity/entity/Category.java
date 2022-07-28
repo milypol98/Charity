ad96f98e3fd8c575b0e6b60b469012ed8ec839bb
milypol98 <milypol98@gmail.com>
2022-07-28 17:58:38 +0200

    first commit

---
+++ src/main/java/pl/coderslab/charity/entity/Category.java
@@ -0,0 +1,16 @@
+package pl.coderslab.charity.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class Category {
+    private int id;
+
+
+}
